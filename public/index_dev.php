9325d13a1883eceeafcb2e62ddb2a42a0636f934
liangcf <lcf@jionx.com>
2020-08-01 18:08:42 +0800

    test

--- public/index_dev.php
+++ public/index_dev.php
@@ -6,7 +6,8 @@
  * 入口文件
  */
 
-use swap\linker\App;
+
+$debugInfo = ['start_time' => $_SERVER['REQUEST_TIME_FLOAT'], 'start_memory' => memory_get_usage()];
 
 header('Content-Type:text/html;charset=UTF-8');
 
@@ -18,4 +19,16 @@ if (is_file(__DIR__ . '/../vendor/autoload.php')) {
     include __DIR__ . '/../vendor/autoload.php';
 }
 
-new App('dev');
+new \swap\core\App('dev');
+
+
+
+$startTime = $debugInfo['start_time'];
+$startMemory = $debugInfo['start_memory'];
+$endTime = microtime(true);
+$endMemory = memory_get_usage();
+$runTime = number_format(($endTime - $startTime), 8) . ' 秒';
+$usedMemory = number_format((($endMemory - $startMemory) / 1024), 6) . ' KB';
+$fileLoadNum = (string)count(get_included_files()) . ' 个';
+echo "<script>console.group('run.info');console.info('运行时间:','{$runTime}');console.info('内存消耗:','{$usedMemory}');console.info('文件数量:','{$fileLoadNum}');console.groupEnd('debug.end');</script>";
+
